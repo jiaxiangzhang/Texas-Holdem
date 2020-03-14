3f27753975051202375641c7782cf8e3c9cd85cc
jiaxiangzhang <2208638304@qq.com>
2020-03-14 14:53:38 +0800

    Log

--- GameTest.java
+++ GameTest.java
@@ -1,51 +1,74 @@
-int level_type1 = 0 , level_type2 = 0;
-	sort(arr1,arr1+5);
-	sort(arr2,arr2+5);
-	level_type1 = get_level_type(arr1);
-	level_type2 = get_level_type(arr2);
-	if (level_type1 > level_type2)
+package cn.Texas Holdem;
+
+import junit.framework.TestCase;
+
+public class GameTest extends TestCase
 	{
-		return -1;
+		public void setUp() throws Exception 
+		{}
+		
+		public void tearDown() throws Exception 
+		{}
+
+		public void testCardTypestraightflush() 
+		{
+		     String s1;
+		     s1="2D 3D 4D 5D 6D ";
+		      CardType instance = new CardType();
+		      int expResult1 = 9;
+		      @SuppressWarnings("static-access")
+			int  result1 = instance.cardType(s1);
+		      System.out.println( result1);
+		      assertEquals(expResult1,  result1);
+		 }
+		public void testCardTypeThreeofaKind() 
+		{
+		     String s2;
+		     s2="2H 4S 4C 1D 4H ";
+		      CardType instance = new CardType();
+		      int expResult2 = 4;
+		      @SuppressWarnings(" static-access")
+			int  result2 = instance.cardType(s2);
+		      System.out.println(result2);
+		      assertEquals(expResult2, result2);
+		 }
+		public void testCardTypeTwoPairs() 
+		{
+		     String s3;
+		     s3="2C 3H 2S 3C KH ";
+		      CardType instance = new CardType();
+		      int expResult2 = 3;
+		      @SuppressWarnings("static-access")
+			int  result2 = instance.cardType(s3);
+		      System.out.println(result2);
+		      assertEquals( expResult2, result2);
+		 }
+		public void testCompare() 
+		{
+		     String s4,s5;
+		     s4="2D 3D 4D 5D 6D ";
+		     s5="3D 4D 5D 6D 7f";
+		      Compare instance = new Compare();
+		      String  expResult = "Black";
+		      @SuppressWarnings("static-access")
+			String result = instance.compare(s4,s5);
+		      System.out.print(result);
+		      assertEquals(expResult, result);
+		 }
+		public void testEnd() 
+		{
+		     String s6,s7;
+		     s6="2D TD 4D 1D 6D ";
+		     s7="3D 4C 5D 6D 7f";
+		      Compare instance = new Compare();
+		      String  expResult = "Black";
+		      @SuppressWarnings("static-access")
+			String result = instance.compare(s6,s7);
+		      System.out.print(result);
+		      assertEquals( expResult, result);
+		 }
+		
 	}
-	else if(level_type1 < level_type2)
-	{
-		return 1;
-	}
-	return  same_level_cmp( arr1,level_type1 ,  arr2, level_type2);
-}
-int main()
-{
-	char str[1000];
-	char *p = NULL;
-	int i = 0;
-	int result = 0;
-	memset(str,0,1000);
-	int array[10] = {0};
-	while(gets(str))
-	{
-		i = 0;
-		p = strtok(str," ");
-		while (p)
-		{
-			array[i++] = rank_card(p[0],p[1]);
-			p = strtok(NULL," ");
-		}
-	//	result =get_result(array,array+5);
-		if ( ( result=get_result(array,array+5)) < 0 )
-		{
-			printf("%s\n",black);
-		}
-		else if(result > 0)
-		{
-			printf("%s\n",white);
-		}
-		else
-		{
-			printf("%s\n",tie);
-		}
-		memset(str,0,1000);
- 
-	}
- 
+
 
 
